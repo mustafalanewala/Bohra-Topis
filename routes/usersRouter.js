827052d2cdefc254c9bbb7fa39e58a6719fd91c8
Mustafa Lanewala <125785421+mustafalanewala@users.noreply.github.com>
2024-08-30 22:50:56 +0530

    setup index.js and user route, register route on postman, impl bcrypt, JWT setup and keys creation, req packages in .env, package for config, created controllers and created test user on postman

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { registerUser } = require("../controllers/authController")
 
 router.get("/", function (req, res) {
     res.send("hey");
 });
 
-module.exports = router;
+router.post("/register", registerUser);
+
+module.exports = router; 
